947dd22b57f1252b1d7c93b9bb1d3686bc8f5fa1
Mark <ripndrug@gmail.com>
2024-11-06 16:18:01 +0200

    modify form page

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,53 @@
-console.log('Form');
+const formData = {
+    email: "",
+    message: "",
+}
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+const localStorageKey = "feedback-form-state";
+
+const savedData = JSON.parse(localStorage.getItem(localStorageKey));
+
+emailInput.value = savedData.email ?? "";
+messageInput.value = savedData.message ?? "";
+// messageInput.value = localStorage.getItem(localStorageKey) ?? "";
+
+// localStorage.setItem(localStorageKey, JSON.stringify(formData));
+
+form.addEventListener('input', onFormInput);
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(e) {
+
+    if (e.target.nodeName !== 'INPUT' && e.target.nodeName !== 'TEXTAREA') {
+        return;
+    }
+
+    if (e.target.nodeName === 'INPUT') {
+        formData.email = e.target.value;
+    }
+
+    if (e.target.nodeName === 'TEXTAREA') {
+        formData.message = e.target.value;
+    }
+
+    localStorage.setItem(localStorageKey, JSON.stringify(formData));
+    console.log(formData)
+
+};
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    if (emailInput.value === "" || messageInput.value === "") {
+        alert(`Fill please all fields`);
+    } else {
+        console.log(formData)
+    }
+    localStorage.removeItem(localStorageKey)
+    formData.email = "";
+    formData.message = "";
+    form.reset();
+}
